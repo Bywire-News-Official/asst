21a0f68427c24539a12c6452de76e25aa844745d
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-12-23 15:10:59 +0000

    new homepage design, really basic

--- asst/src/components/Home.js
+++ asst/src/components/Home.js
@@ -9,16 +9,18 @@ class Home extends Component {
         return (
             <div>
                 
-         
-      
 
                 <Container >
-                <Row className="text-center">
-                <Col lg={{ span: 6, offset: 3 }}>
+                <Row className="text-left">
+                <Col className="p-3" lg={{ span: 6, offset: 3 }}>
                     <br />
-                    <h1>The worlds most advanced A.I Asst.</h1>
-                    <p>AI assistant, or Asst for short, will transform your productivity</p>
-                    <Button href="/xmas-card-writer" variant="success" size="lg">🎄 NEW Christmas Card Writer 🎄</Button>
+                    <h1 className="mb-3">Asst.AI</h1>
+                    <h2 className="mb-3">Your Personal AI Assisant</h2>
+                    <p className="mb-3 lead">Ready to make your holiday season extra special? </p>
+                    <p>Let your new assistant, or asst. for short, take care of all your Christmas card writing needs. Our advanced AI Asst. can help you craft meaningful and thoughtful messages tailored to each of your recipients. </p>
+                        <p>With Asst., you can express your gratitude and appreciation in a personalised, heartfelt, yet timely manner.</p>
+                    <p className="my-3">Get started totally free this Christmas, and make your holiday season even more memorable!</p>
+                    <Button href="/xmas-card-writer" variant="success" size="lg">🎄 Christmas Card Writer 🎄</Button>
                     <br />
                
                     </Col>
@@ -28,20 +30,15 @@ class Home extends Component {
 
                   
 
-                    <img src={homepage} className="fluid" alt="Christmas Card Writer Video" />
+                   
 
 
                    
                 </Row>
                 <Row className="text-left ">
-                    <Col xs={12} md={6}>     <p className="mt-5">Welcome to Asst.ai, the world's most powerful AI writing and list assistance company. We are dedicated to helping our customers save time and streamline their tasks through the use of advanced artificial intelligence technology.</p>
-
-<p>With Asst.ai, you can rely on our AI writing capabilities to tackle a wide range of projects, including business reports, emails, social media posts, and even Christmas cards. Our AI writing technology is able to understand and interpret your unique writing style, making it easy for you to delegate tasks and trust that the end result will meet your high standards.</p>
-
-<p>In addition to our AI writing capabilities, Asst.ai also offers top-notch list assistance services. Our AI algorithms can quickly and accurately organize and manage your to-do lists, shopping lists, and any other lists you need to keep track of. This can help you stay on top of your tasks and make the most of your time.</p>
-
+                    <Col xs={12} md={6}>     
 </Col>
-<Col xs={12} md={6}>  <img width={"100%"} src={twitter} className="fluid" alt="Twitter  Writer Video"/></Col>
+<Col xs={12} md={6}>  </Col>
 
                 </Row>
             </Container>
