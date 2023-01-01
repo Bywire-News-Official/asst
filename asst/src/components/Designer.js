9935cdff834703c2444af38bd0c00617aa3c9677
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2023-01-01 23:28:10 +0000

    update to designer, four images, grid system, modal popup

--- asst/src/components/Designer.js
+++ asst/src/components/Designer.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Container, Form, Button, Card, Row, Col, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Form, Button, Card, Row, Col, Alert, Modal } from 'react-bootstrap';
 import { Configuration, OpenAIApi } from 'openai';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 const API_KEY = process.env.REACT_APP_OPENAI_API_KEY
@@ -7,97 +7,124 @@ const API_KEY = process.env.REACT_APP_OPENAI_API_KEY
 
 function AboutUs() {
 
-  const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [placeholder, setPlaceholder] = useState(
-    "Describe the type of image you would like to create in as much detail as possible"
-  );
-
-    
-    const configuration = new Configuration({
-      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-    });
+    const [prompt, setPrompt] = useState("");
+    const [result, setResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [placeholder, setPlaceholder] = useState(
+      "Describe the type of image you would like to create in as much detail as possible"
+    );
+    const [show, setShow] = useState(false);
+    const [imageURL, setImageURL] = useState('');
   
-    const openai = new OpenAIApi(configuration);
-
-  const generateImage = async () => {
-    setPlaceholder(`Search ${prompt}..`);
-    setLoading(true);
-    const res = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "512x512",
-    });
-    setLoading(false);
-    setResult(res.data.data[0].url);
+      
+      const configuration = new Configuration({
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      });
     
-  };
+      const openai = new OpenAIApi(configuration);
+  
+    const generateImage = async () => {
+      setPlaceholder(`Search ${prompt}..`);
+      setLoading(true);
+      const res = await openai.createImage({
+        prompt: prompt,
+        n: 4,
+        size: "512x512",
+      });
+      setLoading(false);
+      setResult(res.data.data.map(image => image.url));
+      
+    };
     
-        return (
-            <div>
-                 <Container>
-                 <Row className="mt-5">
-            <Col md={{ span: 8, offset: 2 }}>
-    
-    
-                <h1>Image Generator Asst</h1>
-                <br /><br />
-                
-    
-                <br /><br />
-
-                </Col>
-
-                </Row>
-
-                <Row>
-                    <Col md={{ span: 8, offset: 2 }}>
-
-                    
-
-                    <Form>
-    
-    <Form.Group>
-       
-        <Form.Control
-            type="text"
-            name="imageInput"
-            style={{height: "50px"}}
-            placeholder={placeholder} 
-            onChange={(e) => setPrompt(e.target.value)}
-            />
-
-            <Form.Text className="text-muted">
-                Enter as much information as possible for a more accurate image
-            </Form.Text>
-    </Form.Group>
-    </Form>
-            <br />
-    <Button onClick={generateImage} variant="dark gradient" size="lg" type="submit">
-    Generate an Image
-    </Button>
-    {loading ? <ProgressBar animated variant="info" now={100} /> : <div></div> }
-    <br /><br />
-
-    {result.length > 0 ? (
-           <img src={result} alt={prompt} />
-          ) : (
-            <></>
-          )}
-
-    
-                    </Col>
-                </Row>
-
-
-                 </Container>
-            
-            </div>
-
-
-            )
-    }
-
-
-export default AboutUs;
+    const handleClose = () => setShow(false);
+    const handleShow = (imageURL) => {
+      setShow(true);
+      setImageURL(imageURL);
+    };
+          return (
+              <div>
+                   <Container>
+                   <Row className="mt-5">
+              <Col md={{ span: 8, offset: 2 }}>
+      
+      
+                  <h1>Image Generator Asst</h1>
+                  <br /><br />
+                  
+      
+                  <br /><br />
+  
+                  </Col>
+  
+                  </Row>
+  
+                  <Row>
+                      <Col md={{ span: 8, offset: 2 }}>
+  
+                      
+  
+                      <Form>
+      
+      <Form.Group>
+         
+          <Form.Control
+              type="text"
+              name="imageInput"
+              style={{height: "50px"}}
+              placeholder={placeholder} 
+              onChange={(e) => setPrompt(e.target.value)}
+              />
+  
+              <Form.Text className="text-muted">
+                  Enter as much information as possible for a more accurate image
+              </Form.Text>
+      </Form.Group>
+      </Form>
+              <br />
+      <Button onClick={generateImage} variant="dark gradient" size="lg" type="submit">
+      Generate Images
+      </Button>
+      {loading ? <ProgressBar animated variant="info" now={100} /> : <div></div> }
+      <br /><br />
+  
+      {result.length > 0 ? (
+             <Row>
+                 {result.map(image => (
+                     <Col md={3}>
+                         <Card>
+                             <Card.Img variant="top" src={image} onClick={() => handleShow(image)}/>
+                         </Card>
+                     </Col>
+                 ))}
+             </Row>
+            ) : (
+              <></>
+            )}
+  
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Generated Image</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><img src={imageURL} alt={prompt} className="img-fluid" /></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+  
+      
+                      </Col>
+                  </Row>
+  
+  
+                   </Container>
+              
+              </div>
+  
+  
+              )
+      }
+  
+  
+  export default AboutUs;
