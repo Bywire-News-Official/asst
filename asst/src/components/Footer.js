4ef01ceafa20c2084e5315d1495c94d5b942447c
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2023-01-11 01:56:15 +0000

    footer width col 12

--- asst/src/components/Footer.js
+++ asst/src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
 <div class="container-fluid">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div className="container">
-    <div class="col-md-6 d-flex align-items-center">
+    <div class="col-md-12 d-flex align-items-center">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
        
       </a>
